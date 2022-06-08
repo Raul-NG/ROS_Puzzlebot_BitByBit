bd486290ed7bec53c7f30bedbf2d3b46b56f1fe5
George GIO <A01653261@itesm.mx>
2022-06-08 14:40:26 -0500

    New images and code

--- Line_Detection_NOT_ROS/Semaforo/orbmatchingAlmostROS.py
+++ Line_Detection_NOT_ROS/Semaforo/orbmatchingAlmostROS.py
@@ -3,23 +3,21 @@ import numpy as np
 
 tem = []
 glo = []
-tml = 7
+tml = 3
 for i in range(1,tml+1):
-    tem.append(np.pad(cv2.pyrUp(cv2.imread('semaforo_t_%d.png' % i)), pad_width=[(250, 250),(250, 250),(0, 0)], mode='constant',constant_values=(255)))
-    glo.append(cv2.cvtColor(tem[i-1], cv2.COLOR_BGR2GRAY))
+    tem.append(np.pad(cv2.imread('semaforo_t_%d.png' % i), pad_width=[(250, 250),(250, 250),(0, 0)], mode='constant',constant_values=(255)))
+    #tem.append(cv2.imread('semaforo_t_%d.png' % i))
 
-orb = cv2.ORB_create(1000)
-orb.setScaleFactor(1.2)
+orb = cv2.ORB_create()
+orb.setScaleFactor(1.5)
 flann = cv2.FlannBasedMatcher(dict(algorithm = 0, trees = 3), dict(checks = 100))
 
-kpt = []
 dest = []
 for i in range(tml):
-    kptemp, destemp = orb.detectAndCompute(glo[i], None)
-    kpt.append(kptemp)
+    _, destemp = orb.detectAndCompute(tem[i], None)
     dest.append(np.float32(destemp))
 
-colores = ['verde', 'amarillo', 'rojo','apagado 1', 'apagado 2','verde','rojo']
+colores = ['centro','lado','lado']
 cap= cv2.VideoCapture(0)
 slml = []
 il = []
@@ -29,26 +27,26 @@ while True:
     ret, frame= cap.read()
     if cv2.waitKey(1) & 0xFF == 13 or ret == False:
         break
-    gfr = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    kpf, desf = orb.detectAndCompute(gfr, None)
+    _, desf = orb.detectAndCompute(frame, None)
     desf = np.float32(desf)
     slm = 0
     index = 0
     matches = []
     matchesMask = []
-    for j in range(tml):
-        matches.append(flann.knnMatch(dest[j], desf, k=2))
-        matchesMask.append([[0,0] for k in range(len(matches[j]))])
-        for k,(m,n) in enumerate(matches[j]):
-            if m.distance < 0.7*n.distance:
-                matchesMask[j][k]=[1,0]
-        matchesMask[j] = np.array(matchesMask[j])
-        if slm < np.sum(matchesMask[j][:,0]):
-            slm = np.sum(matchesMask[j][:,0])
-            index = j
+    if desf.any():
+        for j in range(tml):
+            matches.append(flann.knnMatch(dest[j], desf, k=2))
+            matchesMask.append([[0,0] for k in range(len(matches[j]))])
+            for k,(m,n) in enumerate(matches[j]):
+                if m.distance < 0.7*n.distance:
+                    matchesMask[j][k]=[1,0]
+            matchesMask[j] = np.array(matchesMask[j])
+            if slm < np.sum(matchesMask[j][:,0]):
+                slm = np.sum(matchesMask[j][:,0])
+                index = j
     slml.append(slm)
     il.append(index)
-    if len(slml) > 19:
+    if len(slml) > 9:
         slml.pop(0)
         il.pop(0)
         slma = round(np.median(np.array(slml)))
@@ -61,7 +59,7 @@ while True:
     thickness = 1
     text1 = cv2.putText(frame, str(slma), org, font, fontScale, color, thickness, cv2.LINE_AA)
     org1 = (0,30)
-    if slma > 1:
+    if slma > 4:
         text2 = cv2.putText(text1, colores[ila], org1, font, fontScale, color, thickness, cv2.LINE_AA)
     else:
         text2 = cv2.putText(text1, "no", org1, font, fontScale, color, thickness, cv2.LINE_AA)
